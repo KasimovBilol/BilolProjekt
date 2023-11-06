89bc44badc3d2d5dae6273ab40f9e2acd7a3f9a1
Kasimov <bilolkasimov2009@gmail.com>
2023-11-06 09:44:18 +0500

    2-Masala

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Uy ishi");
+        int[] array = {1, -2, 0, 3, -4, 0, 5};
+
+        int nol = 0;
+        int musbat = 0;
+        int manfiy = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                nol++;
+            } else if (array[i] > 0) {
+                musbat++;
+            } else {
+                manfiy++;
+            }
+        }
+
+
+        System.out.println("0 lar soni: " + nol);
+        System.out.println("Musbat sonlar soni: " + musbat);
+        System.out.println("Manfiy sonlar soni: " + manfiy);
 
     }
 }
